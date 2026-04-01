19c43e605922e61912f3218004112c60ec9f266d
__int__ <2726087873@qq.com>
2026-04-01 20:25:56 +0800

    init

---
+++ backend/campus-lost-found/src/main/java/dev/forint/campuslostfound/CampusLostFoundApplication.java
@@ -0,0 +1,13 @@
+package dev.forint.campuslostfound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusLostFoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusLostFoundApplication.class, args);
+	}
+
+}
---
+++ backend/campus-lost-found/src/test/java/dev/forint/campuslostfound/CampusLostFoundApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.forint.campuslostfound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusLostFoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
